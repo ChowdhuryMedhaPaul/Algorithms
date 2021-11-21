861bbbf0f1bc2825dd879b0220a861323b9da900
Medha_02 <65821243+ChowdhuryMedhaPaul@users.noreply.github.com>
2021-11-21 21:44:13 +0600

    Add files via upload

---
+++ templet_code(vector).cpp
@@ -0,0 +1,124 @@
+#include<bits/stdc++.h>
+using namespace std;
+//vector < int > fun(vector < int > arr)
+//{
+//    for(int i=0;i<arr.size();i++)
+//        {
+//            arr[i]+=5;
+//            cout<<arr[i]<<endl;
+//        }
+//    return arr;
+//}
+//void fun2(vector < int > &arr)
+//{
+//    for(int i=0;i<arr.size();i++)
+//        {
+//            arr[i]+=5;
+//            cout<<arr[i]<<endl;
+//        }
+//   }
+       int main()
+  {
+//    ///vector_erase...........
+//
+//    vector<int> v1,v2;
+//
+//   int n,n1,n2,ele,a,b,i;
+//    cin>>n;
+//    for(i=0;i<n;i++)
+//       {
+//           cin>>n1>>n2;
+//           v1.push_back(n1);
+//           v2.push_back(n2);
+//       }
+//        if(v1==v2) cout<<"equal"<<endl;
+////        cout<<v.front()+10<<endl;
+////        cout<<v.back()+10<<endl;
+//        v1.swap(v2);
+//          // swap(v1,v2);
+////       cin>>ele;
+////     v.erase(v.begin()+ele-1);
+////       cin>>a>>b;
+////       v.erase(v.begin()+a-1,v.begin()+b-1);
+////       cout<<v.size()<<endl;
+////
+//         for(i=0;i<v1.size();i++)
+//            cout<<v1[i]<<" ";
+//           cout<<endl;
+//           if(v1==v2) cout<<"equal"<<endl;
+//
+//           v1.resize(10,100);
+//            for(i=0;i<v1.size();i++)
+//            cout<<v1[i]<<" ";
+//           cout<<endl;
+////
+//        /......string.....
+//            string a, b, c;
+//    a = "this is a string";
+//    b = a;
+//    c = a + b ;
+//    cout << c << endl;
+//    printf("%s\n", c.c_str() );
+//
+//    cout << c.size() << endl;
+//    for(int i=0; i<c.size(); i++) cout << c[i] ;
+//
+//    /........Stack.....Queue.....
+//        stack< int > st;
+//    st.push( 100 );
+//    st.push( 101 );
+//    st.push( 102 );
+//
+//    while( !st.empty() ) {
+//        cout << st.top() << endl;
+//        st.pop();
+//    }
+//     queue< int > q;
+//    q.push( 100 ); // inserting 100
+//    q.push( 101 ); // inserting 101
+//    q.push( 102 ); // inserting 102
+//
+//    while( !q.empty() ) {
+//        cout << q.front() << endl; // printing the front
+//        q.pop(); // removing that one
+//    }
+//
+//  priority_queue< int > q;
+//    q.push( 10230 ); // inserting 10230
+//    q.push( 1021 ); // inserting 1021
+//    q.push( 102322 ); // inserting 102322
+//
+//    while( !q.empty() ) {
+//        cout << q.top() << endl; // printing the top
+//        q.pop(); // removing that one
+//    }
+//
+//       vector< int > v,temp,temp2;
+//       for(int i=1;i<=10;i++)
+//       v.push_back( i );
+//       temp=fun(v);
+//       fun2(v);
+//       cout<<temp.size()<<endl;
+//       for(int i=0;i<temp.size();i++)
+//        cout<<temp[i]<<endl;
+//         v.insert(v.begin(),100);
+//         v.insert(v.begin()+2,200);
+//         v.erase(v.begin()+2);
+//         v.pop_back();
+//
+//    vector< int > :: iterator it;
+//    for( it = v.begin(); it < v.end(); it++ ) {
+//        printf("%d\n", *it);
+//
+//    }
+//    cout<<v.size()<<endl;
+///........Pair.......
+
+
+  pair< string, int> p("me",12);
+ // cin>>p.first;
+  cout<<p.first<<" "<<p.second<<endl;
+
+
+    return 0;
+}
